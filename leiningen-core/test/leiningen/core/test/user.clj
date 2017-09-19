0a3b6d8686de641ea99b553dc1a8bba97587deaa
brstf <brstf.dev@gmail.com>
2017-09-18 21:08:45 -0400

    Add tests for missing credentials warning printouts

--- leiningen-core/test/leiningen/core/test/user.clj
+++ leiningen-core/test/leiningen/core/test/user.clj
@@ -1,6 +1,7 @@
 (ns leiningen.core.test.user
   (:use clojure.test
-        leiningen.core.user))
+        leiningen.core.user)
+  (:require [clojure.java.io :as io]))
 
 (deftest resolving-repo-creds
   (with-redefs [credentials (constantly {#"^https://clojars\.org/.*"
@@ -36,3 +37,36 @@
               :username "u" :password "p"
               :passphrase "looooong" :private-key-file "./somewhere"
               :foo [:gpg "0x00D85767"]})))))
+
+(defmacro with-err-str
+  "Evaluates exprs in a context in which *err* is bound to a fresh StringWriter.
+   Returns the String created by any printing calls in the evaluated body.
+   Equivalent to with-out-str, but re-binds *err* instead of *out*."
+  [& body]
+  `(let [s# (new java.io.StringWriter)]
+     (binding [*err* s#]
+       ~@body
+       (str s#))))
+
+(deftest missing-credentials-file
+  (with-redefs [leiningen-home (constantly "")
+                io/file (constantly (java.io.File. ""))]
+    (testing "Warning on missing GPG credentials file"
+      (is (.contains (with-err-str (credentials-fn))
+                     "WARNING: Missing GPG credentials file")))))
+
+(deftest missing-credentials
+  (with-redefs [getenv (constantly nil)]
+    (alter-var-root #'leiningen.core.user/warn-missing-credentials-once
+                    (constantly #'leiningen.core.user/warn-missing-credentials))
+    (testing "Missing default environment credential"
+      (is (.contains (with-err-str (resolve-credentials {:username :env}))
+                     ":username as environment variable with name LEIN_USERNAME")))
+    (testing "Missing specific environment credential"
+      (is (.contains (with-err-str (resolve-credentials {:username :env/abc}))
+                     ":username as environment variable with name ABC")))
+    (testing "Missing GPG credential"
+      (with-redefs [credentials (constantly {})]
+        (is (.contains (with-err-str (resolve-credentials {:username :gpg}))
+                       (str ":username as GPG credential with key :username "
+                            "in credentials file:")))))))
